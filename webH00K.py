b4ef15231ef1f47ea6f051f189ad42822c616bec
CS-PRIO30 <cs.prio30@gmail.com>
2017-08-24 16:49:41 +0200

    Update webH00K.py

--- webH00K.py
+++ webH00K.py
@@ -18,15 +18,6 @@ updater.bot.set_webhook("https://webh00k.herokuapp.com/" + TOKEN)
 updater.start_webhook(listen="0.0.0.0",
                       port=PORT,
                       url_path=TOKEN)
-
-def echo(bot, update):
-  bot.sendMessage(chat_id=update.message.chat_id, text=update.message.text)
-  
-def start(bot, update):
-  bot.sendMessage(chat_id=update.message.chat_id, text="ciao!")
-  
-def hello(bot, update):
-  bot.sendMessage(chat_id=update.message.chat_id, text=update.message.text)
   
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
