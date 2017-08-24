83b8ab890d41ee4536ca4ca11fa2ebf0d2762569
CS-PRIO30 <cs.prio30@gmail.com>
2017-08-24 14:33:08 +0200

    Update webH00K.py

--- webH00K.py
+++ webH00K.py
@@ -10,4 +10,5 @@ updater.start_webhook(listen="0.0.0.0",
                       port=PORT,
                       url_path=TOKEN)
 updater.bot.set_webhook("https://webh00k.herokuapp.com/" + TOKEN)
+print("url set")
 updater.idle()
