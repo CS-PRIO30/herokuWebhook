5f27f76658764dd5a35e16f91c30164c13c47d76
CS-PRIO30 <cs.prio30@gmail.com>
2017-08-24 15:13:48 +0200

    Update webH00K.py

--- webH00K.py
+++ webH00K.py
@@ -6,9 +6,12 @@ TOKEN = os.environ['TOKEN_TELEGRAM']
 PORT = int(os.environ['PORT'])
 updater = Updater(TOKEN)
 # add handlers
+
+updater.bot.set_webhook("https://webh00k.herokuapp.com/" + TOKEN)
+
 updater.start_webhook(listen="0.0.0.0",
                       port=PORT,
                       url_path=TOKEN)
-updater.bot.set_webhook("https://webh00k.herokuapp.com/" + TOKEN)
+
 print("url set")
 updater.idle()
